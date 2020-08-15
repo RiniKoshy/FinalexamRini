663e7e74e53947cec6253a7a8adf5f9d83acfc95
RiniKoshy <koshyrini@gmail.com>
2020-08-15 10:21:43 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Finalnet22
+{
+	public class Program
+	{
+		public static void Main()
+		{
+			
+			var person = new Person();
+			person.name = "Rini Koshy";
+			var alarm = new AlarmClock();
+			alarm.AlarmClockEvent += person.HandleAlarm;
+			alarm.Alarm();
+		}
+	}
+
+	public class Person
+	{
+		public string name { get; set; }
+
+		public void HandleAlarm(object sender, AlarmClockEventArgs e)
+		{
+			Console.WriteLine("Get up Rini it's {0}", e.time);
+		}
+
+	}
+
+	
+	public class AlarmClock
+	{
+		public event AlarmClockEventHandeler AlarmClockEvent;
+
+		public void Alarm()
+		{
+			AlarmClockEventHandeler alarm = AlarmClockEvent;
+			if (AlarmClockEvent != null)
+			{
+				alarm(this, new AlarmClockEventArgs(DateTime.Now));
+			}
+
+		}
+	}
+
+	public delegate void AlarmClockEventHandeler(object source, AlarmClockEventArgs e);
+
+	public class AlarmClockEventArgs : EventArgs
+	{
+		public DateTime time { get; set; }
+		public AlarmClockEventArgs(DateTime time)
+		{
+			this.time = time;
+
+		}
+	}
+}
